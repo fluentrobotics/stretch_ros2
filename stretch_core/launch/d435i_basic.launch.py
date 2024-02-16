33bb4fd7a56b625981e640bcf2a6d104367b1866
Elvin Yang <eyy@umich.edu>
2024-02-16 17:25:22 -0500

    Fix realsense accel error

--- stretch_core/launch/d435i_basic.launch.py
+++ stretch_core/launch/d435i_basic.launch.py
@@ -12,7 +12,7 @@ json_path = os.path.join(get_package_share_directory('stretch_core'), 'config', 
 configurable_parameters = [{'name': 'camera_namespace',             'default': '', 'description': 'namespace for camera'},
                            {'name': 'camera_name',                  'default': 'camera', 'description': 'camera unique name'},
                            {'name': 'json_file_path',               'default': json_path, 'description': 'allows advanced configuration'},
-                           {'name': 'depth_module.profile',         'default': '848x480x15', 'description': 'depth module profile'},                           
+                           {'name': 'depth_module.profile',         'default': '848x480x15', 'description': 'depth module profile'},
                            {'name': 'enable_depth',                 'default': 'true', 'description': 'enable depth stream'},
                            {'name': 'rgb_camera.profile',           'default': '1280x720x15', 'description': 'color image width'},
                            {'name': 'enable_color',                 'default': 'true', 'description': 'enable color stream'},
@@ -20,17 +20,18 @@ configurable_parameters = [{'name': 'camera_namespace',             'default': '
                            {'name': 'enable_infra2',                'default': 'false', 'description': 'enable infra2 stream'},
                            {'name': 'infra_rgb',                    'default': 'false', 'description': 'enable infra2 stream'},
                            {'name': 'enable_confidence',            'default': 'false', 'description': 'enable depth stream'},
-                           {'name': 'gyro_fps',                     'default': '200', 'description': "''"},                           
-                           {'name': 'accel_fps',                    'default': '63', 'description': "''"},                           
-                           {'name': 'enable_gyro',                  'default': 'true', 'description': "''"},                           
-                           {'name': 'enable_accel',                 'default': 'true', 'description': "''"},                           
-                           {'name': 'pointcloud.enable',            'default': 'true', 'description': ''}, 
+                           {'name': 'gyro_fps',                     'default': '200', 'description': "''"},
+                           # TODO(fluent): default value of 63 causes the driver to complain; the error message resets the value to 100
+                           {'name': 'accel_fps',                    'default': '100', 'description': "''"},
+                           {'name': 'enable_gyro',                  'default': 'true', 'description': "''"},
+                           {'name': 'enable_accel',                 'default': 'true', 'description': "''"},
+                           {'name': 'pointcloud.enable',            'default': 'true', 'description': ''},
                            {'name': 'pointcloud.stream_filter',     'default': '2', 'description': 'texture stream for pointcloud'},
                            {'name': 'pointcloud.stream_index_filter','default': '0', 'description': 'texture stream index for pointcloud'},
-                           {'name': 'enable_sync',                  'default': 'true', 'description': "''"},                           
-                           {'name': 'align_depth.enable',           'default': 'true', 'description': "''"},                           
-                           {'name': 'initial_reset',                'default': 'true', 'description': "''"},                           
-                           {'name': 'allow_no_texture_points',      'default': 'true', 'description': "''"},                           
+                           {'name': 'enable_sync',                  'default': 'true', 'description': "''"},
+                           {'name': 'align_depth.enable',           'default': 'true', 'description': "''"},
+                           {'name': 'initial_reset',                'default': 'true', 'description': "''"},
+                           {'name': 'allow_no_texture_points',      'default': 'true', 'description': "''"},
                           ]
 
 def declare_configurable_parameters(parameters):
@@ -49,7 +50,7 @@ def generate_launch_description():
      d435i_accel_correction = Node(
           package='stretch_core',
           executable='d435i_accel_correction',
-          output='screen',
+          output='log',
           )
 
      return LaunchDescription(declare_configurable_parameters(configurable_parameters) + [
